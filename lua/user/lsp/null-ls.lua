a651e546462d64dabb0ceed0cca35f41d5dd3b4f
Nick Harris <nick@nickharris.dev>
2022-10-23 22:33:24 +0100

    formats on save

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -13,12 +13,22 @@ null_ls.setup {
   debug = false,
   sources = {
     formatting.prettier.with {
-      extra_filetypes = { "toml" },
-      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+      extra_filetypes = { "toml" }
     },
-    formatting.black.with { extra_args = { "--fast" } },
-    formatting.stylua,
-    formatting.google_java_format,
-    diagnostics.flake8,
+    diagnostics.eslint,
+    formatting.stylua
   },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+          vim.lsp.buf.formatting_sync()
+        end,
+      })
+    end
+  end,
 }
