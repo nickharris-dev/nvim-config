ef50b99ccb6fbfba6cd0c1b4105bb7d0cbb7a355
Nick Harris <nick@nickharris.dev>
2022-10-25 13:54:08 +0100

    dracula colours to rainbow and lualine

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -8,8 +8,10 @@ if not status_ok then
 	return
 end
 
+local rainbow_colors = require('dracula').colors()
+
 configs.setup({
-  ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python" }, -- put the language you want in this array
+  ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "typescript", "tsx", "scss" }, -- put the language you want in this array
   -- ensure_installed = "all", -- one of "all" or a list of languages
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
@@ -21,7 +23,7 @@ configs.setup({
 	autopairs = {
 		enable = true,
 	},
-	indent = { enable = true, disable = { "python", "css" } },
+	indent = { enable = true, disable = { "css" } },
 
 	context_commentstring = {
 		enable = true,
@@ -33,7 +35,7 @@ configs.setup({
     -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
     extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
     max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
+    colors = rainbow_colors, -- table of hex strings
     -- termcolors = {} -- table of colour name strings
   }
 })
