60ae11ef0e557586dbc21d74e39af1ad01c6a5d5
Nick Harris <nick.harris@juro.com>
2022-11-08 10:49:20 +0000

    adds options to dracula colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,13 @@
 local colorscheme = "dracula"
 
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+local status_ok, dracula = pcall(require, colorscheme)
 if not status_ok then
-  return
+	return
 end
+
+dracula.setup({
+	transparent_bg = true,
+	italic_comment = true,
+})
+
+local _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
