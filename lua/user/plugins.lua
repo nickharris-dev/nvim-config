cc1a0f110c4cfc6505026c888defe6bc682ecafb
Nick Harris <nick.harris@juro.com>
2022-11-08 16:34:40 +0000

    adds jest snippets

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -73,6 +73,7 @@ return packer.startup(function(use)
 
 	-- snippets
 	use({ "L3MON4D3/LuaSnip", commit = "8f8d493e7836f2697df878ef9c128337cbf2bb84" }) --snippet engine
+	use({ "andys8/vscode-jest-snippets" })
 	use({ "rafamadriz/friendly-snippets", commit = "2be79d8a9b03d4175ba6b3d14b082680de1b31b1" }) -- a bunch of snippets to use
 
 	-- LSP
